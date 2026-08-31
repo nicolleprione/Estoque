349055547551d02806e2b5b8557002f3d0040ab5
Nicolle Prione <nicolleprione@gmail.com>
2026-08-31 19:42:15 -0300

    começo do projeto

---
+++ app.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title='Estoquito')
+
+# Teste e criação da variável do código selecionado
+if 'codigo_selecionado' not in st.session_state:
+    st.session_state.codigo_selecionado = None
+
+# Título
+st.markdown("""
+# Bem-Vindo(a) ao Estoquito
+""")
+
+# Importação dos dados
+importar_arquivo = st.file_uploader(label='Importar', type=['csv'])
+
+# Verifica se foi importado algo
+if importar_arquivo:
+
+    # Leitura dos dados
+    df = pd.read_csv(importar_arquivo, dtype={'Codigo': str})
+    
+    # Remoção das duplicatas
+    df_limpo = df.drop_duplicates(subset='Codigo')
+
+    # Informações da planilha
+    total_original = len(df)
+    total_unicos = len(df_limpo)
+    duplicados = total_original - total_unicos
+    st.write(f'Total de registros: {total_original}')
+    st.write(f'Códigos únicos: {total_unicos}')
+    st.write(f'Duplicados: {duplicados}')
+
+    st.subheader('Lista de Conferência')
+
+    # Seleção de itens
+    for _, item in df_limpo.iterrows():
+        codigo = item['Codigo']
+        descricao = item['Descricao']
+        locacao = item['Locacao']
+        st.write(f'{codigo} - {descricao}')
+
+        if st.button('Conferir', key=codigo):
+            st.session_state.codigo_selecionado = codigo
+
+        st.divider() # linha separadora
+
+    # Exibindo o Item
+    if st.session_state.codigo_selecionado:
+        codigo = st.session_state.codigo_selecionado
+        item = df_limpo[df_limpo['Codigo'] == codigo].iloc[0]
+
+        st.subheader('Item Selecionado')
+        st.write(f'Código: {item['Codigo']}')
+        st.write(f'Descrição: {item['Descricao']}')
+        st.write(f'Locação: {item['Locacao']}')
