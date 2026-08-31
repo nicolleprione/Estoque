4d68bef80829bd83fd7c0584b8d1abf81f51606f
Nicolle Prione <nicolleprione@gmail.com>
2026-08-31 20:15:26 -0300

    mudança de leitura csv para xlsx, e mudanças para as colunas da planilha

--- app.py
+++ app.py
@@ -13,32 +13,32 @@ st.markdown("""
 """)
 
 # Importação dos dados
-importar_arquivo = st.file_uploader(label='Importar', type=['csv'])
+importar_arquivo = st.file_uploader(label='Importar', type=['xlsx'])
 
 # Verifica se foi importado algo
 if importar_arquivo:
 
     # Leitura dos dados
-    df = pd.read_csv(importar_arquivo, dtype={'Codigo': str})
+    df = pd.read_excel(importar_arquivo, dtype={'Códg Mestre': str})
     
     # Remoção das duplicatas
-    df_limpo = df.drop_duplicates(subset='Codigo')
+    df_limpo = df.drop_duplicates(subset='Códg Mestre')
 
     # Informações da planilha
     total_original = len(df)
     total_unicos = len(df_limpo)
     duplicados = total_original - total_unicos
-    st.write(f'Total de registros: {total_original}')
-    st.write(f'Códigos únicos: {total_unicos}')
-    st.write(f'Duplicados: {duplicados}')
+    st.write(f"Total de registros: {total_original}")
+    st.write(f"Códigos únicos: {total_unicos}")
+    st.write(f"Duplicados: {duplicados}")
 
     st.subheader('Lista de Conferência')
 
     # Seleção de itens
     for _, item in df_limpo.iterrows():
-        codigo = item['Codigo']
-        descricao = item['Descricao']
-        locacao = item['Locacao']
+        codigo = item['Códg Mestre']
+        descricao = item['Descrição']
+        locacao = item['Locações']
         st.write(f'{codigo} - {descricao}')
 
         if st.button('Conferir', key=codigo):
@@ -49,9 +49,9 @@ if importar_arquivo:
     # Exibindo o Item
     if st.session_state.codigo_selecionado:
         codigo = st.session_state.codigo_selecionado
-        item = df_limpo[df_limpo['Codigo'] == codigo].iloc[0]
+        item = df_limpo[df_limpo['Códg Mestre'] == codigo].iloc[0]
 
         st.subheader('Item Selecionado')
-        st.write(f'Código: {item['Codigo']}')
-        st.write(f'Descrição: {item['Descricao']}')
-        st.write(f'Locação: {item['Locacao']}')
+        st.write(f"Código: {item['Códg Mestre']}")
+        st.write(f"Descrição: {item['Descrição']}")
+        st.write(f"Locação: {item['Locações']}")
