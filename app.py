8be600d2900e4304b07aca4f137cd45d0f69c4a2
Nicolle Prione <nicolleprione@gmail.com>
2026-09-01 21:20:10 -0300

    adição de bordas, alinhamento dos botões

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ if importar_arquivo:
         progresso = 0
 
     st.progress(progresso)
-    st.write(f'{concluidos} de {total_itens} intens conferidos.')
+    st.write(f'{concluidos} de {total_itens} itens conferidos.')
 
     st.subheader('Lista de Conferência')
 
@@ -66,6 +66,8 @@ if importar_arquivo:
 
     # Seleção de itens
     for _, item in itens_ordenados.iterrows():
+        # Geração de colunas
+        coluna1, coluna2 = st.columns([3,1], vertical_alignment='center')
 
         codigo = item['Códg Mestre']
         descricao = item['Descrição']
@@ -73,79 +75,79 @@ if importar_arquivo:
 
         # Item conferido
         if codigo in st.session_state.conferencias:
-            st.success(f'✓ {codigo} - {descricao}')
+            with coluna1:
+                st.success(f'✓ {codigo} - {descricao}')
 
             # Botão editar
-            if st.button('Editar', key=f'editar_{codigo}'):
-                st.session_state.codigo_selecionado = codigo
-                st.session_state.codigo_confirmado = True
-                st.session_state.modo_edicao = True
+                with coluna2:
+                    if st.button('Editar', key=f'editar_{codigo}'):
+                        st.session_state.codigo_selecionado = codigo
+                        st.session_state.codigo_confirmado = True
+                        st.session_state.modo_edicao = True
 
-                st.rerun()
+                        st.rerun()
 
         # Item não conferido
         else:
-            st.write(f'{codigo} - {descricao}')
+            with coluna1:
+                st.write(f'{codigo} - {descricao}')
+            with coluna2:
+                if st.button('Conferir', key=codigo):
+                    st.session_state.codigo_selecionado = codigo
+                    st.session_state.codigo_confirmado = False
+                    st.session_state.modo_edicao = False
 
-            if st.button('Conferir', key=codigo):
-                st.session_state.codigo_selecionado = codigo
-                st.session_state.codigo_confirmado = False
-                st.session_state.modo_edicao = False
-
-                st.rerun()
+                    st.rerun()
 
         # Se o item foi selecionado
         if st.session_state.codigo_selecionado == codigo:
-            st.subheader('Item Selecionado')
-            st.write(f"Código: {item['Códg Mestre']}")
-            st.write(f"Descrição: {item['Descrição']}")
-            st.write(f"Locação: {item['Locações']}")
+            with st.container(border=True):
+                st.write(f"Código: {item['Códg Mestre']}")
+                st.write(f"Descrição: {item['Descrição']}")
+                st.write(f"Locação: {item['Locações']}")
 
             # Confirmação do Item
-            if not st.session_state.modo_edicao:
-                st.subheader('Confirmação do código')
-
-                codigo_digitado = st.text_input('Digite o código: ', key=f'codigo_digitado_{codigo}')
-            
-                if st.button('Confirmar código', key=f'confirmar_{codigo}'):
-                    if codigo_digitado == codigo:
-                        st.session_state.codigo_confirmado = True
-                        st.rerun()
-                    else:
-                        st.session_state.codigo_confirmado = False
-                        st.error('Código incorreto.')
+                if not st.session_state.modo_edicao:
+                    codigo_digitado = st.text_input('Confirme o código: ', key=f'codigo_digitado_{codigo}')
+                
+                    if st.button('Confirmar código', key=f'confirmar_{codigo}'):
+                        if codigo_digitado == codigo:
+                            st.session_state.codigo_confirmado = True
+                            st.rerun()
+                        else:
+                            st.session_state.codigo_confirmado = False
+                            st.error('Código incorreto.')
 
              # Conferencia
             if st.session_state.codigo_confirmado:
-                st.subheader('Dados de conferência')
-            
-                conferencia_existente = st.session_state.conferencias.get(codigo)
+                with st.container(border=True):
+                    conferencia_existente = st.session_state.conferencias.get(codigo)
 
-                # Valores inicias
-                if conferencia_existente:
-                    conferencia_inicial = conferencia_existente['conferente']
-                    contagem_inicial = conferencia_existente['contagem']
-                else:
-                    conferencia_inicial = 'Selecione'
-                    contagem_inicial = 0
-            
-                funcionarios = ['Selecione','Ana Clara', 'André']
-                indice_conferente = funcionarios.index(conferencia_inicial)
-            
-                conferente = st.selectbox('Usuário', funcionarios, index=indice_conferente)
-            
-                contagem = st.number_input('Quantidade física', min_value=0, step=1, value=int(contagem_inicial))
-            
-                if st.button('Salvar'):
-                    if conferente == 'Selecione':
-                        st.error('Selecione um usuário')
+                    # Valores inicias
+                    if conferencia_existente:
+                        conferencia_inicial = conferencia_existente['conferente']
+                        contagem_inicial = conferencia_existente['contagem']
                     else:
-                        st.session_state.conferencias[codigo] = {'conferente':conferente, 'contagem': contagem}
+                        conferencia_inicial = 'Selecione'
+                        contagem_inicial = 0
+                
+                    funcionarios = ['Selecione','Ana Clara', 'André']
+                    indice_conferente = funcionarios.index(conferencia_inicial)
+                
+                    conferente = st.selectbox('Usuário', funcionarios, index=indice_conferente, key=f'usuario_{codigo}')
+                
+                    contagem = st.number_input('Quantidade física', min_value=0, step=1, value=int(contagem_inicial), key=f'contagem_{codigo}')
+                
+                    if st.button('Salvar'):
+                        if conferente == 'Selecione':
+                            st.error('Selecione um usuário')
+                        else:
+                            st.session_state.conferencias[codigo] = {'conferente':conferente, 'contagem': contagem}
 
-                        st.session_state.codigo_selecionado = None
-                        st.session_state.codigo_confirmado = False
-                        st.session_state.modo_edicao = False
-            
-                        st.rerun()
+                            st.session_state.codigo_selecionado = None
+                            st.session_state.codigo_confirmado = False
+                            st.session_state.modo_edicao = False
+                
+                            st.rerun()
 
         st.divider() # linha separadora
